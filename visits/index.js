35fcfe8d31c19636431e8a89088894f019eb79af
Jimmy Tang <blay.tenshi@gmail.com>
2019-04-20 18:05:45 +0800

    Basic docker-compose app up and running!

--- visits/index.js
+++ visits/index.js
@@ -2,7 +2,16 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+    // if we weren't using docker we would have had to specify an
+    // address such as 'localhost:6379' or 'http://myredis.com:6379'
+    // but since we are using docker, we can just provide it with the
+    // service name specified in the yml file!
+    // It's actually docker that recognises this url/uri so you can
+    // use it in place of even database url/uris!
+    host: 'redis-server',
+    port: 6379 // default redis port
+});
 client.set('visits', 0);
 
 // app that serves the number of visits to a front end app
